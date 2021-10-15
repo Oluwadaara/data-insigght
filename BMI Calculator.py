95d84bf8d981615483f2d0da997f548211ac91bc
Oluwadaara <89576468+Oluwadaara@users.noreply.github.com>
2021-10-15 16:30:25 +0100

    bmi calculator app jupyter notebook and script

---
+++ BMI Calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#                             BODY MASS INDEX (BMI) CALCULATOR
+# 
+# Body Mass Index (BMI) is a measurement of a person’s weight with respect to his or her height. It is more of an indicator than a direct measurement of a person’s total body fat.
+# 
+# BMI, more often than not, correlates with total body fat. This means that as the BMI score increases, so does a person’s total body fat.
+# 
+# Body Mass Index (BMI) is a person’s weight in kilograms divided by the square of height in meters. A high BMI can indicate high body fatness. BMI screens for weight categories that may lead to health problems, but it does not diagnose the body fatness or health of an individual.
+# Formula for BMI = weight in kilograms * square of height in meters
+# 
+
+# In[5]:
+
+
+weight = input("Enter your weight in KG:")
+height = input("Enter your height in meteres:")
+BMI = float(weight)/ float(height)**2
+BMI
+
+
+# In[ ]:
+
+
+
+
